17db36b5f8d55d5be4834c28b58187ec7b8fbd86
Matheus <131068785+Matheus-m-prado@users.noreply.github.com>
2023-05-19 19:17:33 -0300

    resumo desmatamento em verde

--- script.js
+++ script.js
@@ -21,8 +21,12 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 // SCROLL
-window.sr = ScrollReveal({ reset: true });
-sr.reveal(".scroll", {duration: 1300, rotate: {x: 0, y: 80, z: 0}});
+window.sr = ScrollReveal({ reset: false });
+sr.reveal(".scroll", {duration: 1300, distance: '70px', origin: 'bottom', easing: 'cubic-bezier(0.5, 0, 0, 1)', opacity: 0});
+sr.reveal(".scroll2", {duration: 1300, distance: '70px', origin: 'top', easing: 'cubic-bezier(0.5, 0, 0, 1)', opacity: 0});
+sr.reveal(".scroll3", {duration: 1300, distance: '70px', origin: 'left', easing: 'cubic-bezier(0.5, 0, 0, 1)', opacity: 0});
+sr.reveal(".scroll4", {duration: 1300, distance: '70px', origin: 'right', easing: 'cubic-bezier(0.5, 0, 0, 1)', opacity: 0});
+
 
 
 
